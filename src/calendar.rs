89c17576c966c26e3cb48b05acfea1ec6fa8047b
zak <90650199+zkayia@users.noreply.github.com>
2024-01-10 21:23:59 +0100

    reduce calendar updates log

--- src/calendar.rs
+++ src/calendar.rs
@@ -54,19 +54,17 @@ async fn update_calendar() -> anyhow::Result<()> {
     logln!("- Fetching {}...", group.name);
     let calendar = fetch(&client, &group.student_id, &start, &end).await?;
 
-    logln!("- Adding {} to database...", group.name);
+    logln!("  - Updating database...");
     db_update_calendar(&mut db_con, &group.name, &calendar)?;
-
-    logln!("- Generating {}.ics...", group.name);
-    generate_ics(&calendar, PUBLIC_PATH.join([&group.name, "ics"].join(".")))?;
-    
-    logln!("- Generating {}.json...", group.name);
-    generate_json(&calendar, PUBLIC_PATH.join([&group.name, "json"].join(".")))?;
     
     let group_dir_path = PUBLIC_PATH.join(&group.name);
     
-    logln!("- Creating group directory...");
+    logln!("  - Creating group directory...");
     fs::create_dir_all(&group_dir_path)?;
+
+    logln!("  - Generating ics and json files...");
+    generate_ics(&calendar, PUBLIC_PATH.join([&group.name, "ics"].join(".")))?;
+    generate_json(&calendar, PUBLIC_PATH.join([&group.name, "json"].join(".")))?;
 
     static CATEGORIES: [&str; 3] = ["CM", "TD", "Examen"];
     
@@ -75,10 +73,7 @@ async fn update_calendar() -> anyhow::Result<()> {
       let events = &calendar.iter().filter(|e| e.celcat.event_category.starts_with(category)).cloned().collect();
       let lowercase_category = category.to_lowercase();
 
-      logln!("- Generating {}.ics...", lowercase_category);
       generate_ics(events, group_dir_path.join([&lowercase_category, "ics"].join(".")))?;
-
-      logln!("- Generating {}.json...", lowercase_category);
       generate_json(events, group_dir_path.join([&lowercase_category, "json"].join(".")))?;
     }
 
@@ -86,13 +81,10 @@ async fn update_calendar() -> anyhow::Result<()> {
       |e| CATEGORIES.iter().all(|c| !e.celcat.event_category.starts_with(c))
     ).cloned().collect();
     
-    logln!("- Generating autre.ics...");
     generate_ics(other_events, group_dir_path.join("autre.ics"))?;
-
-    logln!("- Generating autre.json...");
     generate_json(other_events, group_dir_path.join("autre.json"))?;
 
-    logln!("- Generating {}.png", group.name);
+    logln!("  - Generating png file");
     generate_png(
       &db_get_period(&db_con, &group.name, &get_week_bounds(&Local::now()))?,
       &group.name
